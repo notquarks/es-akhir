e7597a0efbc87872ea5e20bf760e0b1cd161873a
Abhyasa Danu B <abhyasadanub@gmail.com>
2023-06-20 15:12:08 +0700

    add quiz

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,22 +1,57 @@
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 import {
   Box,
+  Button,
+  Center,
   Container,
+  Divider,
   Flex,
   Grid,
   GridItem,
   HStack,
+  Heading,
+  ListItem,
   Progress,
+  Radio,
+  RadioGroup,
   Stack,
   Text,
+  UnorderedList,
+  useRadioGroup,
 } from "@chakra-ui/react";
+// import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Quiz = () => {
-  const progress_value = 50;
+  const [questions, setQuestions] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const questionsList = questions.slice(questionIndex, questionIndex + 4);
+  // const [answers, setAnswers] = useState([]);
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestion = () => {
+      try {
+        fetch("/questions")
+          .then((response) => response.json())
+          .then((data) => setQuestions(data), setQuestionIndex(0));
+        // const jsonData = await response.json();
+        // setQuestions(jsonData.data);
+        // console.log(jsonData.data);
+      } catch (error) {
+        console.error("Error fetching quest:", error);
+      }
+    };
+    fetchQuestion();
+  }, []);
+
+  const progress_value = Math.round((questionIndex / 44) * 100);
   return (
     <Flex
-      m={"auto"}
       flexDir={"column"}
-      w="100vw"
+      // w="100vw"
+      flex={1}
+      m={"auto"}
       alignItems="center"
       justifyContent={"space-around"}
     >
@@ -29,14 +64,19 @@ const Quiz = () => {
         </Stack>
       </Box>
       <Box
-        w={"100%"}
+        // w={"100%"}
         py={4}
-        flexWrap={"wrap"}
         direction={"column"}
         alignItems={"center"}
       >
-        <Stack>
-          <Box w={"60%"} alignItems={"center"} m={"auto"} pb={24}>
+        <Flex direction={"column"} flex={1} minWidth={"max-content"}>
+          <Box
+            w={"60vw"}
+            alignItems={"center"}
+            m={"auto"}
+            pb={24}
+            minWidth={"max-content"}
+          >
             <Grid templateColumns="60px 2fr" alignItems={"center"}>
               <GridItem>
                 <Text fontSize={"xl"}>{progress_value}%</Text>
@@ -51,7 +91,90 @@ const Quiz = () => {
               </GridItem>
             </Grid>
           </Box>
-        </Stack>
+          <Flex direction={"column"} align={"center"}>
+            {questionsList.map((question) => (
+              <Box key={question.id} alignItems={"center"}>
+                <Heading as="h3" size="lg" py={4} color={"dimgray"}>
+                  {question.question}
+                </Heading>
+                <Center>
+                  <RadioGroup
+                    id={question.id}
+                    alignItems={"center"}
+                    justifyContent={"space-around"}
+                    py={8}
+                    // onChange={setValue}
+                    // value={value}
+                  >
+                    <HStack spacing={12}>
+                      <Radio
+                        size="xxl"
+                        name={"ans" + question.id}
+                        value="1"
+                        onChange={(event) => {
+                          setAnswers([...answers, event.target.value]);
+                        }}
+                      ></Radio>
+                      <Radio
+                        size="xl"
+                        name={"ans" + question.id}
+                        value="2"
+                        onChange={(event) => {
+                          setAnswers([...answers, event.target.value]);
+                        }}
+                      ></Radio>
+                      <Radio
+                        size={"lg"}
+                        name={"ans" + question.id}
+                        value="3"
+                        onChange={(event) => {
+                          setAnswers([...answers, event.target.value]);
+                        }}
+                      ></Radio>
+                      <Radio
+                        size="xl"
+                        name={"ans" + question.id}
+                        value="4"
+                        onChange={(event) => {
+                          setAnswers([...answers, event.target.value]);
+                        }}
+                      ></Radio>
+                      <Radio
+                        size="xxl"
+                        name={"ans" + question.id}
+                        value="5"
+                        onChange={(event) => {
+                          setAnswers([...answers, event.target.value]);
+                          console.log(answers.length);
+                        }}
+                      ></Radio>
+                    </HStack>
+                  </RadioGroup>
+                </Center>
+                <Divider />
+              </Box>
+            ))}
+            <Button
+              isDisabled={answers.length != questionIndex ? true : false}
+              mt={6}
+              colorScheme="teal"
+              // size="lg"
+              h={"80px"}
+              w={"264px"}
+              rightIcon={<ArrowForwardIcon />}
+              borderRadius={"full"}
+              fontSize={"2xl"}
+              onClick={() => {
+                // const answer = questionsList[questionIndex].id;
+                // setAnswers([...answers, answer]);
+                setQuestionIndex(questionIndex + 4);
+                console.log(answers);
+              }}
+            >
+              Next
+            </Button>
+          </Flex>
+        </Flex>
       </Box>
     </Flex>
   );
