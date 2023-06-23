0ad8e041d329ee1bef7a183380b61b639d48904f
Abhyasa Danu B <abhyasadanub@gmail.com>
2023-06-23 16:56:43 +0700

    use floor n ceil instead

--- api/api.py
+++ api/api.py
@@ -9,6 +9,7 @@ import json
 import joblib
 import time
 import os
+import math
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 
@@ -65,7 +66,7 @@ class ExpertSystem:
         distances.sort(key=lambda x: x[0])
         k = int(np.sqrt(len(case_answers)))
         print('k :',k)
-        k = k + 1 if k % 2 == 0 else k 
+         k = math.ceil(k) if k % 2 == 0 else math.floor(k)
         print('k after:',k)
         k_nearest = distances[:k]
             
